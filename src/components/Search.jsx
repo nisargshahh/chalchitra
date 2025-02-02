f8945243ab140eecfe9f9d6c9c6ffb6fa74e3acf
Nisarg Shah <devnisargshah@gmail.com>
2025-02-03 01:26:55 +0530

    first commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+
+const Search = ({ searchTerm, setsearchTerm }) => {
+  return (
+    <div className="search">
+      <div className="">
+        <img src="search.svg" alt="search" />
+        <input
+          type="text"
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setsearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
