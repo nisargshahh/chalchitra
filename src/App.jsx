d78fbf39166be6610efe9a739a6face6a4abcb85
Nisarg Shah <devnisargshah@gmail.com>
2025-02-21 12:48:35 +0530

    Temp

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Home from "./pages/Home.jsx"
+import Actors from "./pages/Actors.jsx"
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -6,6 +7,7 @@ const App = () => {
     <div>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/actors" element={<Actors/>}/>
       </Routes>
     </div>
   );
