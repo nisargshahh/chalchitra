d7ce39eddf7e99ef15f2605c4c498c88fd74c2db
Nisarg Shah <devnisargshah@gmail.com>
2025-02-21 12:51:29 +0530

    Temp

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <div>
+        <section className="footer">
+          <div className="text-l text-[#C6DE41] font-bold mt-20">
+            <h3>
+              <a href="https://github.com/nisargshahh">
+                Nisarg Shah &#169; 2025
+              </a>
+            </h3>
+          </div>
+        </section>
+    </div>
+  )
+}
+
+export default Footer
